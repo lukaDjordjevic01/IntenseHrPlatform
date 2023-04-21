5428c85903785ef6e418d3991ae77080402ec60c
Luka Djordjevic <lukadjordjevicsm01@gmail.com>
2023-04-21 22:56:44 +0200

    Initial commit

---
+++ src/main/java/com/example/internshipIntense/InternshipIntenseApplication.java
@@ -0,0 +1,13 @@
+package com.example.internshipIntense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipIntenseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipIntenseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/internshipIntense/InternshipIntenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshipIntense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipIntenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
